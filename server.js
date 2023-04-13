d5a3fd5d55135c4e98ac9279e5ba505ade8e44cd
Glitch (glitch-blank-node) <none>
2023-04-13 06:25:19 +0000

    💶🎃 Checkpoint ./src/pages/login.hbs:54002983/250 ./server.js:54002983/27

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ fastify.route({
     // Comprobar si el usuario está autenticado
     if (!request.session.user) {
       // Devolver un error si el usuario no está autenticado
-      return done(new Error("Ya se ha iniciado sesión"));
+      // return done(new Error("Ya se ha iniciado sesión"));
     }
     // Continuar con la solicitud si el usuario está autenticado
     done();
