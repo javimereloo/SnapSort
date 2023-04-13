169b8373d76de4f82e9fb8d80b10943ef91353f8
Glitch (glitch-blank-node) <none>
2023-04-13 06:58:25 +0000

    🚣🏦 Checkpoint ./server.js:54002983/2194 ./src/pages/main.hbs:54002983/20

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ fastify.route({
     done();
   },
   handler: (request, reply) => {
-    return reply.view("/src/pages/login.hbs");
+    return reply.view("/src/pages/login.hbs")
   },
 });
 
@@ -66,15 +66,12 @@ fastify.route({
   method: "POST",
   url: "/login",
   preHandler: (request, reply, done) => {
-    console.log(`USER: ${request.body.username}`)
-    console.log(`passwd: ${request.body.password}`)
-    //TODO 
-    // // Comprobar si el usuario está autenticado
-    // if (!request.session.user) {
-    //   // Devolver un error si el usuario no está autenticado
-    //    return done(new Error('Ya se ha iniciado sesión'))
-    // }
-    // // Continuar con la solicitud si el usuario está autenticado
+    //TODO Comprobar usuario y contraseña
+    //Cifrar contraseña
+    // Comprobar si el usuario está autenticado
+    if (request.session.user) {
+       return done(new Error('Ya se ha iniciado sesión'))
+    }
     done()
   },
   handler: (request, reply) => {
@@ -82,11 +79,26 @@ fastify.route({
       name: request.body.username,
       password: request.body.password,
     };
-    if (user) {
-      // Establecer la información de sesión para el usuario
-      request.session.user = user;
-      reply.send({ message: "Inicio de sesión exitoso" });
+    request.session.user = user;
+    reply.view("/src/pages/main.hbs", user)
+    
+  },
+});
+
+//Route to acccess register view
+fastify.route({
+  method: "GET",
+  url: "/logout",
+  preHandler: (request, reply, done) => {
+    // Comprobar si el usuario está autenticado
+    if (!request.session.user) {
+      return reply.redirect("/");
     }
+     reply.session.destroy()
+  },
+  handler: (req, reply) => {
+    // Manejar la solicitud del perfil del usuario
+    return reply.view("/");
   },
 });
 
