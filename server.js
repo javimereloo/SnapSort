ac4d034694b118ec8bd24957f07effb508f263d8
Glitch (glitch-blank-node) <none>
2023-04-13 06:47:23 +0000

    💪💗 Checkpoint ./server.js:54002983/483 ./src/pages/login.hbs:54002983/4779

--- server.js
+++ server.js
@@ -65,15 +65,18 @@ fastify.route({
 fastify.route({
   method: "POST",
   url: "/login",
-  // preHandler: (request, reply, done) => {
-  //   // Comprobar si el usuario está autenticado
-  //   if (!request.session.user) {
-  //     // Devolver un error si el usuario no está autenticado
-  //      return done(new Error('Ya se ha iniciado sesión'))
-  //   }
-  //   // Continuar con la solicitud si el usuario está autenticado
-  //   done()
-  // },
+  preHandler: (request, reply, done) => {
+    console.log(`USER: ${request.body.username}`)
+    console.log(`passwd: ${request.body.password}`)
+    //TODO 
+    // // Comprobar si el usuario está autenticado
+    // if (!request.session.user) {
+    //   // Devolver un error si el usuario no está autenticado
+    //    return done(new Error('Ya se ha iniciado sesión'))
+    // }
+    // // Continuar con la solicitud si el usuario está autenticado
+    done()
+  },
   handler: (request, reply) => {
     const user = {
       name: request.body.username,
