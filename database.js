b52cf3faa4c8a84abda1bc6be6b03fc5c8c48f4c
Glitch (glitch-blank-node) <none>
2023-04-13 18:37:02 +0000

    🌄💈 Checkpoint ./database.js:54002983/759

---
+++ database.js
@@ -0,0 +1,27 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// Crear una nueva base de datos en memoria
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Conectado a la base de datos SQLite.');
+});
+
+// Crear una tabla en la base de datos
+db.run('CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, email TEXT, password TEXT)', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Tabla creada en la base de datos.');
+});
+
+// Cerrar la conexión a la base de datos cuando finalice el proceso
+process.on('exit', () => {
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Conexión a la base de datos cerrada.');
+  });
+});
