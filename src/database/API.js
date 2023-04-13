f03112ee7bc17bd546093c94ee81af812404ac2d
Glitch (glitch-blank-node) <none>
2023-04-13 20:00:01 +0000

    🛳🕋 Checkpoint ./src/database/API.js:54002983/5805

--- src/database/API.js
+++ src/database/API.js
@@ -1,11 +1,11 @@
-//METHODS TO ACCESS THE DB 
-const sqlite3 = require('sqlite3').verbose()
-const bcrypt = require('bcrypt')
-const db = require('./db.config.js')
+//METHODS TO ACCESS THE DB
+const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcrypt");
+const db = require("./db.config.js");
 
 //Get user info from ID
 function getUserById(id, callback) {
-  db.get('SELECT * FROM users WHERE id = ?', [id], function(err, row) {
+  db.get("SELECT * FROM users WHERE id = ?", [id], function (err, row) {
     if (err) {
       callback(err);
     } else if (!row) {
@@ -16,20 +16,41 @@ function getUserById(id, callback) {
   });
 }
 
-
 //Insert a new user
-function insertUser(name, email, password){
-  
+function insertUser(name, email, password) {
+  db.run(
+    "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+    [name, email, password],
+    function (err) {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(`Se ha insertado ${this.changes} fila`);
+      }
+    }
+  )
 }
 
-
-//cryptographic methods
-function crypt(){
-  
-}
 
 
 //Export the API OPERATIONS
 module.exports = {
   getUserById: getUserById,
 };
+
+
+
+
+//-------------------------Auxiliar cryptographic methods-----------------------
+//Function thats returns the hash of a password
+function crypt(password) {
+  bcrypt.hash(password, 10, function (err, hash) {
+    if (err) {
+      console.error(err.message);
+      return null;
+    } else {
+      console.log(`La contraseña cifrada es: ${hash}`);
+      return hash;
+    }
+  });
+}
