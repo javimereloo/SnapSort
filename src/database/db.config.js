1ee4977f1203553c3018796dea1166e78abcc3f2
Glitch (glitch-blank-node) <none>
2023-04-15 17:15:56 +0000

    🏣👖 Checkpoint ./src/database/db.config.js:54002983/2664

--- src/database/db.config.js
+++ src/database/db.config.js
@@ -1,45 +1,43 @@
 //THIS FILE MAY BE EXECUTED ONE TO CREATE THE INSTANCE
-//AND THE NECESSARY TABLES 
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
+//AND THE NECESSARY TABLES
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
-const dbPath = path.join(__dirname, 'sqlite3', 'SNAPSORT.sqlite');
+const dbPath = path.join(__dirname, "SNAPSORT.sqlite");
 
-
-// Create a new database 
+// Create a new database
 let DB = new sqlite3.Database(dbPath, (err) => {
   if (err && err.code == "SQLITE_CANTOPEN") {
-    createDatabase();
     console.error(err.message);
-  }else{
-    console.log('Conectado a la base de datos SQLite.');
+  } else {
+    console.log("Conectado a la base de datos SQLite.");
   }
 });
 
-// Create a table for user information 
-DB.run(`CREATE TABLE usuario (
-          username TEXT PRIMARY KEY AUTOINCREMENT, 
+// Create a table for user information
+DB.run(
+  `CREATE TABLE IF NOT EXISTS usuario (
+          username TEXT PRIMARY KEY, 
           name TEXT, 
           lastname TEXT, 
           email TEXT , 
-          password TEXT)`
-       , (err) => {
-  if (err) {
-    console.error(err.message);
+          password TEXT)`,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    } 
   }
-  console.log('Tabla creada en la base de datos.');
-});
+);
 
 //TODO COMPROBAR SI ES NECESARIO
 // Cerrar la conexión a la base de datos cuando finalice el proceso
-process.on('exit', () => {
+process.on("exit", () => {
   DB.close((err) => {
     if (err) {
       console.error(err.message);
     }
-    console.log('Conexión a la base de datos cerrada.');
+    console.log("Conexión a la base de datos cerrada.");
   });
 });
-
 
 module.exports = DB;
