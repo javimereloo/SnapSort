b5041df1912a7365b3d8dedb4f19011fad4516d7
Glitch (snapsort) <none>
2023-04-24 06:26:16 +0000

    🌇🍯 Checkpoint ./package.json:54002983/257 ./spec/server.spec.js:54002983/318

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -2,6 +2,8 @@ const supertest = require("supertest");
 const fastify   = require("../server.js");
 const server = fastify.server;
 
+
+
 describe("Probando rutas servidor ", () => {
   it("should return login page", (done) => {
     supertest(server)
