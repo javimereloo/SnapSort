66a26781a0354033dda208a132f7ea5ac99c1055
Glitch (snapsort) <none>
2023-04-25 16:32:28 +0000

    🏘🔦 Checkpoint ./credentials.json:54002983/4693 ./src/GoogleDrive/googleAPI.js:54002983/9

--- src/GoogleDrive/googleAPI.js
+++ src/GoogleDrive/googleAPI.js
@@ -4,7 +4,7 @@ const { google } = require('googleapis');
 const credentials = require('../../credentials.json');
 const auth = new google.auth.GoogleAuth({
   credentials: credentials,
-  scopes: ['https://www.googleapis.com/auth/drive.readonly']
+  scopes: ['https://www.googleapis.com/auth/drive']
 });
 const drive = google.drive({ version: 'v3', auth });
 
