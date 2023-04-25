edc8856571493bfa9037d78da57c54d641a309e2
Glitch (snapsort) <none>
2023-04-25 08:12:21 +0000

    🌞👋 Checkpoint ./src/database/API.js:54002983/270

--- src/database/API.js
+++ src/database/API.js
@@ -172,7 +172,7 @@ function deleteUser(username) {
 //Import google API to access to folders and files
 const { google } = require('googleapis');
 const auth = new google.auth.GoogleAuth({
-  keyFile: './credential.json',
+  keyFile: process.env.GOOGLE_CREDENTIALS_PATH,
   scopes: ['https://www.googleapis.com/auth/drive.readonly'],
 });
 const drive = google.drive({ version: 'v3', auth });
