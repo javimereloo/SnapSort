2861a9ed37c3848722dcc0d07bc02c2e29c11dc2
Glitch (snapsort) <none>
2023-05-02 09:48:58 +0000

    🚞🦀 Checkpoint ./spec/server.spec.js:54002983/4216 ./server.js:54002983/30

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,15 +1,14 @@
 const supertest = require("supertest");
-const app   = require("../server.js");
+const app = require("../server.js");
 const server = app.server;
 
 describe("Probando rutas servidor ", () => {
-  it('GET / returns status 200', async () => {
-    const response = await supertest(server).get('/');
-    expect(response.statusCode).toBe(200);
+  it("GET / returns status 200", async (done) => {
+    supertest(server)
+      .get("/index")
+      .expect(200)
+      .expect(function (res) {
+        console.log("BODY ACERCA DE ", res.body); // Para comprobar qué contiene exactamente res.body
+      });
   });
 });
-
-
- // afterAll((done) => {
- //    server.close(done);
- //  });
