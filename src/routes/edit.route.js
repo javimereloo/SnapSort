27ef25ee96358da2eebe9cc7a9b334c5cb9f7586
Glitch (snapsort) <none>
2023-05-04 06:22:40 +0000

    🚡👄 Checkpoint ./src/pages/edit.hbs:54002983/812 ./src/routes/edit.route.js:54002983/1163

--- src/routes/edit.route.js
+++ src/routes/edit.route.js
@@ -10,6 +10,38 @@ module.exports = async function (fastify, opts) {
         const errorMessage = true;
         return reply.redirect(`/login?errorMessage=${errorMessage}`);
       }
+      if (!API.getOwner(imagenID) === request.session.user.username || !) {
+        return reply.redirect("/home");
+      }
+  
+      done();
+    },
+    handler: async (request, reply) => {
+      const imagenID = decodeURIComponent(request.params.imagenID);
+      let imagenInfo;
+      await API.getImageInfo(imagenID)
+        .then((imageInfo) => {
+          imagenInfo = imageInfo;
+          console.log(imagenInfo);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      return reply.view("/src/pages/edit.hbs", {
+        user: request.session.user,
+        imagenInfo: imagenInfo,
+      });
+    },
+  });
+  fastify.route({
+    method: "POST",
+    url: "/edit/:imagenID",
+    preHandler: (request, reply, done) => {
+      const imagenID = decodeURIComponent(request.params.imagenID);
+      if (!request.session.user) {
+        const errorMessage = true;
+        return reply.redirect(`/login?errorMessage=${errorMessage}`);
+      }
       if (!API.getOwner(imagenID) === request.session.user.username) {
         return reply.redirect("/home");
       }
